3bd746d90b1ce2ebf3369e342701b7634bf6fe1c
Seitah Abdullah <iseitah@outlook.com>
2019-05-29 14:35:54 +0300

    nothing

--- new design/js/index.js
+++ new design/js/index.js
@@ -1,31 +1,72 @@
 //Strict whole script
    'use strict';
 
-   $( document ).ready(function() {
-    console.log( "ready!" );
+//    $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
+
+
+$(document).ready(function(){
+    $('#eid').click(function(){
+     //do something
+     console.log(" eid");
+    });
+});
+
+$(document).ready(function(){
+    $('#eid').click(function(){/*do something*/
+      // ************************ Start
+        var elementClicked;
+        $('#eid').click(function(){
+           elementClicked = true;
+        });
+        $('#ramadan').click(function(){
+           elementClicked = true;
+        });
+
+        if( elementClicked != true ) {
+          for (var i = 1; i <= 3; i++) {
+            $('#backgrounds').append('<img onclick="changeIt(this)" class="bg img-responsive" src="eid designs/eid_backgrounds/'+i+'.jpg" />');
+          }
+            console.log(" eid");
+
+        }
+        else {
+          for (var i = 1; i <= 16; i++) {
+            $('#backgrounds').append('<img onclick="changeIt(this)" class="bg img-responsive" src="eid designs/backgrounds/'+i+'.jpg" />');
+          }
+            console.log(" ramadan");
+        }
+      // end ************************
+    });
 });
 
 
+
+// window.addEventListener("load", function(){
+//      document.getElementById("RootNode").onclick = function(){/*do something*/};
+// });
+
 // ************************ Start
-  var elementClicked;
-  $('#eid').click(function(){
-     elementClicked = true;
-  });
-  $('#ramadan').click(function(){
-     elementClicked = true;
-  });
-
-  if( elementClicked != true ) {
-    for (var i = 1; i <= 3; i++) {
-      $('#backgrounds').append('<img onclick="changeIt(this)" class="bg img-responsive" src="eid designs/eid_backgrounds/'+i+'.jpg" />');
-    }
-      console.log(" eid");
-
-  }
-  else {
-    for (var i = 1; i <= 16; i++) {
-      $('#backgrounds').append('<img onclick="changeIt(this)" class="bg img-responsive" src="eid designs/backgrounds/'+i+'.jpg" />');
-    }
-      console.log(" ramadan");
-  }
+  // var elementClicked;
+  // $('#eid').click(function(){
+  //    elementClicked = true;
+  // });
+  // $('#ramadan').click(function(){
+  //    elementClicked = true;
+  // });
+  //
+  // if( elementClicked != true ) {
+  //   for (var i = 1; i <= 3; i++) {
+  //     $('#backgrounds').append('<img onclick="changeIt(this)" class="bg img-responsive" src="eid designs/eid_backgrounds/'+i+'.jpg" />');
+  //   }
+  //     console.log(" eid");
+  //
+  // }
+  // else {
+  //   for (var i = 1; i <= 16; i++) {
+  //     $('#backgrounds').append('<img onclick="changeIt(this)" class="bg img-responsive" src="eid designs/backgrounds/'+i+'.jpg" />');
+  //   }
+  //     console.log(" ramadan");
+  // }
 // end ************************
